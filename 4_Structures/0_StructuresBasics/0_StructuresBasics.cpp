9a9517fb5d663506a7ba7be5e7a19b761440a2be
LukasZdrazilek <147374351+LukasZdrazilek@users.noreply.github.com>
2023-10-17 08:57:30 +0000

    Nahrani ukolu 1, 2, 3

--- 4_Structures/0_StructuresBasics/0_StructuresBasics.cpp
+++ 4_Structures/0_StructuresBasics/0_StructuresBasics.cpp
@@ -16,8 +16,8 @@ void changePersonAge(Person* person, int age);
 int main()
 {
     Person person1;
-    sprintf_s(person1.first_name, 20, "Pepa");
-    sprintf_s(person1.last_name, 20, "Omacka");
+    //sprintf_s(person1.first_name, 20, "Pepa");
+    //sprintf_s(person1.last_name, 20, "Omacka");
     person1.age = 20;
 
     Person person2 = {
