8dbe6af6e590019e7ebd01ad773813e61db243a2
TomasRacil <72918819+TomasRacil@users.noreply.github.com>
2023-10-30 07:06:09 +0000

    Ukazka fungovani git

--- Vyuka/LinkedList/source.cpp
+++ Vyuka/LinkedList/source.cpp
@@ -10,22 +10,40 @@ struct Node
 
 void printNode(Node* node);
 Node* newNode(int val);
-
+Node* addNextNode(int val, Node* node1);
 
 int main()
 {
 
-    Node* node1 = newNode(10);
+    Node* node1 = newNode(1);
+    cout<<"Node1:"<<endl;
     printNode(node1);
-    Node* node2 = newNode(10);
+
+    Node* node2 = newNode(2);
+    cout<<endl<<"Node2:"<<endl;
     printNode(node2);
+
     node1->next=node2;
+    cout<<endl<<"Node1&2:"<<endl;
     printNode(node1);
+
+    Node* node3 = addNextNode(3, node1);
+    cout<<endl<<"Node1&2&3:"<<endl;
+    printNode(node1);
+    
+
 
 	return 0;
 	
 }
 
+Node* addNextNode(int val, Node* previous){
+    Node* newNode = new Node();
+    newNode->val;
+    newNode->next = previous->next;
+    previous->next = newNode;
+    return newNode;
+}
 
 void printNode(Node* node)
 {
